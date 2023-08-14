782d0a72d8413d6ef36694fed2f2dc5655fab58a
Vijay Sutar <vijay@parasightsolutions.com>
2023-08-14 19:26:11 +0530

    Brands list fetched from the api

--- lib/brands.dart
+++ lib/brands.dart
@@ -11,33 +11,72 @@ class Brands extends StatefulWidget{
 }
 
 class BrandList extends State<Brands>{
+
+
+  void initState(){
+    super.initState();
+    getApiData();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Brands')
       ),
-      body: getBrandsList(),
+      body: Column(
+        children: [
+          getList()
+        ],
+      ),
     );
   }
 
-  Future<ListView> getBrandsList() async {
+  Widget getList(){
+    return Expanded(
+      child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (BuildContext, int index)
+    {
+      return ListView.separated(itemBuilder: (context, index){
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(arrNames[index].brand_name, style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),),
+            );
+          },
+          itemCount: arrNames.length,
+            separatorBuilder: (BuildContext context, int index) {
+            return Divider(height: 30, thickness: 2,);
+            },
+          );
+    }),
+    );
+  }
 
+
+  Future<void> getApiData() async{
     String url = "${constants.apiBaseURL}/brand";
-    var results = await http.get(Uri.parse(url));
-    var arrNames = jsonDecode(results.body);
-    return ListView.separated(itemBuilder: (context, index){
-      return Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(arrNames[index].brand_name, style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),),
-      );
-    },
-    itemCount: arrNames.length,
-      separatorBuilder: (BuildContext context, int index) {
-      return Divider(height: 30, thickness: 2,);
-      },
-    );
-
+    var result = await http.get(Uri.parse(url));
+    var arrNames = jsonDecode(result.body);
+    print(arrNames);
   }
+
+  // ListView getBrandsList() {
+  //
+  //   String url = "${constants.apiBaseURL}/brand";
+  //   var results = http.get(Uri.parse(url));
+  //   var arrNames = jsonDecode(results);
+  //   return ListView.separated(itemBuilder: (context, index){
+  //     return Padding(
+  //       padding: const EdgeInsets.all(8.0),
+  //       child: Text(arrNames[index].brand_name, style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),),
+  //     );
+  //   },
+  //   itemCount: arrNames.length,
+  //     separatorBuilder: (BuildContext context, int index) {
+  //     return Divider(height: 30, thickness: 2,);
+  //     },
+  //   );
+  //
+  // }
 
 }
