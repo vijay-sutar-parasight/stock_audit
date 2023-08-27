4c654bc687571ff00dd80086d1004b5916eff2c6
Vijay Sutar <vijay@parasightsolutions.com>
2023-08-27 22:14:01 +0530

    data sync in progress

--- lib/datasync/datasync.dart
+++ lib/datasync/datasync.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class DataSync extends StatelessWidget{
+class DataSync extends StatefulWidget{
+  @override
+  State<DataSync> createState() => _DataSyncState();
+}
+
+class _DataSyncState extends State<DataSync> {
+  bool brands = true;
+
+  bool formats = true;
+
+  bool variants = true;
+
+  bool descriptions = true;
+
+  bool warehouses = true;
+
+  bool audit = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,48 +26,74 @@ class DataSync extends StatelessWidget{
         title: Text('Database Sync', style: TextStyle(color: Colors.white)),
         centerTitle: true,
       ),
-      body: Container(
-        child: GridView.count(crossAxisCount: 2,
+    body: Column(
+      children: [
+        CheckboxListTile(
+            title: Text("Brands"),
+            value: brands, onChanged: (val){
+          setState((){
+                brands = val!;
+              });
+        }),
+        CheckboxListTile(
+            title: Text("Formats"),
+            value: formats, onChanged: (val){
+          setState((){
+            formats = val!;
+          });
+        }),
+        CheckboxListTile(
+            title: Text("Variants"),
+            value: variants, onChanged: (val){
+          setState((){
+            variants = val!;
+          });
+        }),
+        CheckboxListTile(
+            title: Text("Descriptions"),
+            value: descriptions, onChanged: (val){
+          setState((){
+            descriptions = val!;
+          });
+        }),
+        CheckboxListTile(
+            title: Text("Warehouse"),
+            value: warehouses, onChanged: (val){
+          setState((){
+            warehouses = val!;
+          });
+        }),
+        CheckboxListTile(
+            title: Text("Audit & Audit Entries"),
+            value: audit, onChanged: (val){
+          setState((){
+            audit = val!;
+          });
+        }),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: InkWell(
-                onTap: (){
-                  //Navigator.push(context, MaterialPageRoute(builder: (context) => Brands()));
-                },
-                child: Card(color: Colors.white,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.branding_watermark_rounded, size: 80,color: Colors.lightBlue,),
-                      Text('Master Data Sync',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: InkWell(
-                onTap: (){
-                 // Navigator.push(context, MaterialPageRoute(builder: (context) => Formats()));
-                },
-                child: Card(color: Colors.white,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.list_rounded, size: 80,color: Colors.lightBlue,),
-                      Text('Audit Data Sync',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            Flexible(
+              child: ElevatedButton(onPressed: (){
 
+              }, child: Text(
+                  'Import From Server'
+              )),
+            ),
+            SizedBox(width: 20,),
+            Flexible(
+              child: ElevatedButton(onPressed: (){
+                print(brands);
+              }, child: Text(
+                  'Sync To Server'
+              )),
+            )
           ],
         ),
-      ),
+        
+
+      ],
+    )
     );
   }
-
 }
